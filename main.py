0bda6aea804e57fc62d26efaaad700cb00039663
d-g-code <>
2021-10-26 16:22:07 +0200

    Added new words from file 'words'

--- main.py
+++ main.py
@@ -15,14 +15,18 @@ CREATE TABLE IF NOT EXISTS database_words (
 connection = create_connection('database_words.db')
 execute_query(connection, create_table_repeating_word)
 
-# create_word = """
-# INSERT INTO
-#     database_words (word_eng, word_pol, amount_repeat)
-# VALUES
-#     ('tree', 'drzewo', 0),
-#     ('grass', 'trawa', 0)
-# """
-# execute_query(connection, create_word)
+# READ FILE WITH WORDS, AND SET DEFAULT VALUE
+# f = open('words', 'r')
+# lines = f.readlines()
+# for line in range(len(lines)):
+#     row = lines[line]
+#     cluster = row[0:-1].split()
+#     new_word_eng = cluster[0]
+#     new_word_pol = ' '.join(cluster[1:])
+#     create_word = "INSERT INTO " \
+#                   "database_words (word_eng, word_pol, amount_repeat, repeat_correct_session ) " \
+#                   "VALUES ('{}', '{}', 0, 0)".format(new_word_eng, new_word_pol)
+#     execute_query(connection, create_word)
 
 # select all repeating word
 select_repeating_words = "SELECT * from database_words"
@@ -37,8 +41,8 @@ def repeat(rw):
             print(colored('GOOD', 'green'), '\n')
         else:
             print(colored('BAD', 'red'))
-            print(colored(rw[row][1], 'green'))
-            print(colored(user_answer, 'blue'), '\n')
+            print(colored(rw[row][1].upper(), 'green'))
+            print(colored(user_answer.upper(), 'blue'), '\n')
 
 
 repeat(repeating_words)
