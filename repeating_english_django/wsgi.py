cea481c299d5a1c9e0006123b73397a2fe90fcf5
d-g-code <>
2022-02-28 01:03:43 +0100

    Django project initial

---
+++ repeating_english_django/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for repeating_english_django project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'repeating_english_django.settings')
+
+application = get_wsgi_application()
---
+++ repeating_english_django_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class RepeatingEnglishDjangoAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'repeating_english_django_app'
