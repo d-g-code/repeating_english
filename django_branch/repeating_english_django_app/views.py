f0936a0c07752cc7c1a5514f7815e734e623555c
d-g-code <>
2022-03-15 14:58:55 +0100

    Signup / register view,url,html

--- django_branch/repeating_english_django_app/views.py
+++ django_branch/repeating_english_django_app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+
+def signup(request):
+    form = UserCreationForm(request.POST or None)
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render('repeating_english_django_app/signup.html')
+        else:
+            return render(request, 'repeating_english_django_app/signup.html', {'form': form})
+    else:
+        return render(request, 'repeating_english_django_app/signup.html', {'form': form})
